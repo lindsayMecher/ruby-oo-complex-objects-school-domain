77b686e59e1b4e07f1e9b5dc21f474b4109ff740
lindsayMecher <lindsaymecher@gmail.com>
2021-02-02 16:07:54 -0600

    commit tree 29820f952867ad020365df3c26eaec6a477bdd41

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+
+class School 
+
+    attr_accessor :name, :roster
+
+
+    def initialize name
+        @name = name
+        @roster = {}
+    end
+
+    def add_student name, grade 
+        binding.pry
+        self.roster[grade] ? self.roster[grade] << name : self.roster[grade] = [name]
+    end 
+
+    def grade num
+        self.roster[num]
+    end 
+
+    def sort
+        roster = {}
+        sorted_names = self.roster.each do |k,v|
+            roster[k] = v.sort
+        end
+        roster.sort_by do |k,v|
+            k
+        end.to_h
+    end 
+end 
